a7c341c1b5c54b14f1e1a5b4348b3b3b31c7e43f
Allen Liu <kamyu104@gmail.com>
2017-04-09 18:29:03 +0800

    update

--- Qualification Round/fashion-show.py
+++ Qualification Round/fashion-show.py
@@ -4,77 +4,46 @@
 # https://code.google.com/codejam/contest/3264486/dashboard#s=p3
 #
 # Time:  O(N^2)
-# Space: O(N^2)
+# Space: O(N)
 #
-
-from collections import Counter, defaultdict
-    
-def add(style, i, j, val, row, col, diag, anti):
-    row[style][i] += val
-    col[style][j] += val
-    diag[style][i-j] += val
-    anti[style][i+j] += val
-
-def update(old, style, i, j, row, col, diag, anti):
-    add(old, i, j, -1, row, col, diag, anti)
-    add(style, i, j, 1, row, col, diag, anti)
-    
-def is_valid(i, j, row, col, diag, anti):
-    if row['o'][i] + row['x'][i] > 1 or \
-       col['o'][j] + col['x'][j] > 1 or \
-       diag['o'][i-j] + diag['+'][i-j] > 1 or \
-       anti['o'][i+j] + anti['+'][i+j] > 1:
-        return False
-    return True
-
-def fill_stage(stage, i, j, row, col, diag, anti, points, result):
-    for style in ('o', '+', 'x'):
-        if (stage[i][j] == '.') or \
-           (stage[i][j] in ('+', 'x') and style == 'o'):
-            old = stage[i][j]
-            stage[i][j] = style
-            points += 1 if stage[i][j] != 'o' else 2
-            update(old, style, i, j, row, col, diag, anti)
-            if is_valid(i, j, row, col, diag, anti):
-                 result.append((style, i, j))
-                 break
-            update(style, old, i, j, row, col, diag, anti)
-            points -= 1 if stage[i][j] != 'o' else 2
-            stage[i][j] = old
-    else:
-        points += stage[i][j] != '.' if stage[i][j] != 'o' else 2
-    return points
 
 def fashion_show():
     N, M = map(int, raw_input().strip().split())
-    stage = [['.' for _ in xrange(N)] for _ in xrange(N)]
-    row, col = defaultdict(Counter), defaultdict(Counter)
-    diag, anti = defaultdict(Counter), defaultdict(Counter)
+    stage, row, col, diag, anti = set(), set(), set(), set(), set()
     for _ in xrange(M):
         style, i, j = raw_input().strip().split()
-        i, j = int(i)-1, int(j)-1
-        stage[i][j] = style
-        add(style, i, j, 1, row, col, diag, anti)
-    
-    points, result = 0, []
-    left, right, top, bottom = 0, N-1, 0, N-1        
-    while left <= right and top <= bottom: # spiral order
-        for j in xrange(left, right + 1):
-            points = fill_stage(stage, top, j, row, col, diag, anti, points, result)
-        for i in xrange(top + 1, bottom):
-            points = fill_stage(stage, i, right, row, col, diag, anti, points, result)
-        for j in reversed(xrange(left, right + 1)):
-            if top < bottom:
-                points = fill_stage(stage, bottom, j, row, col, diag, anti, points, result)
-        for i in reversed(xrange(top + 1, bottom)):
-            if left < right:
-                points = fill_stage(stage, i, left, row, col, diag, anti, points, result)
-        left, right, top, bottom = left + 1, right - 1, top + 1, bottom - 1
-    return points, result
+        i, j = int(i), int(j)
+        stage.add((i, j))
+        if style != '+':
+            row.add(i)
+            col.add(j)
+        if style != 'x':
+            diag.add(i-j)
+            anti.add(i+j)
+
+    modles = []
+    for i in xrange(1,N+1):
+        for p in [(i,j) for j in xrange(i,N+1)]+[(j,i) for j in xrange(i+1,N+1)]:
+            addTimes, addPlus = False, False
+            if (p[0] not in row) and (p[1] not in col):
+                addTimes = True
+                row.add(p[0])
+                col.add(p[1])
+            if  (p[0]-p[1] not in diag) and (p[0]+p[1] not in anti):
+                addPlus = True
+                anti.add(p[0]+p[1])
+                diag.add(p[0]-p[1])
+            if (addTimes and addPlus) or ((addTimes or addPlus) and p in stage):
+                modles.append(('o',p[0],p[1]))
+            elif addTimes:
+                modles.append(('x',p[0],p[1]))
+            elif addPlus:
+                modles.append(('+',p[0],p[1]))
+    return len(row)+len(diag), modles
    
 for case in xrange(input()):
-    points, result = fashion_show()
-    print 'Case #%d: %d %d' % (case+1, points, len(result))
-    for style, i, j in result:
-        print style, i+1, j+1
+    points, modles = fashion_show()
+    print 'Case #%d: %d %d' % (case+1, points, len(modles))
+    for style, i, j in modles:
+        print style, i, j
     
