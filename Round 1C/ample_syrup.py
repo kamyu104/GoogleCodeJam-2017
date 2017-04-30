1ecabe8d4559ec72e7d6665871b624965e75489e
Allen Liu <kamyu104@gmail.com>
2017-04-30 20:11:00 +0800

    add

---
+++ Round 1C/ample_syrup.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2017 kamyu. All rights reserved.
+#
+# Google Code Jam 2017 Round 1C - Problem A. Ample Syrup
+# https://code.google.com/codejam/contest/3274486/dashboard#s=p0
+#
+# Time:  O(N * logK)
+# Space: O(K)
+#
+
+from math import pi
+from heapq import heappush, heappop
+
+def ample_syrup():
+    N, K = map(int, raw_input().strip().split())
+    Ps = []
+    for i in xrange(N):
+        Ps.append(map(int, raw_input().strip().split()))
+    Ps.sort()
+
+    result, min_heap = 0, []
+    for P in Ps:
+        if len(min_heap) == K-1:
+            result = max(result, P[0] * P[0] + 2 * P[0] * P[1] + sum(min_heap))
+        heappush(min_heap, 2 * P[0] * P[1])
+        if len(min_heap) >= K:
+            heappop(min_heap)
+    return result * pi
+
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, ample_syrup())
