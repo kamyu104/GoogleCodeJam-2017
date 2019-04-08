0d7945fb2433ff5f573a6de76085210928f87a90
kamyu <kamyu104@users.noreply.github.com>
2019-04-09 00:56:19 +0800

    Update bathroom-stalls.py

--- Qualification Round/bathroom-stalls.py
+++ Qualification Round/bathroom-stalls.py
@@ -7,25 +7,34 @@
 # Space: O(1)
 #
 
+#  we can reduce the problem by the following rules:
+#  - bathroom_stalls(2N+2, 2K+2) = bathroom_stalls(N+1, K+1)
+#  - bathroom_stalls(2N+3, 2K+2) = bathroom_stalls(N+1, K+1)
+#  - bathroom_stalls(2N+2, 2K+3) = bathroom_stalls(N,   K+1)
+#  - bathroom_stalls(2N+3, 2K+3) = bathroom_stalls(N+1, K+1)
+#  - bathroom_stalls(2N+2,    1) = (N+1, N)
+#  - bathroom_stalls(2N+1,    1) = (N,   N)
+
+def bathroom_stalls2():
+    N, K = map(int, raw_input().strip().split())
+    while K > 1:
+        if N % 2 == 0 and K % 2 == 1:
+            N -= 1
+        N, K = N//2, K//2
+
+    return (N//2, N//2) if N % 2 == 1 else (N//2, N//2-1)
+
 def max_min(n):
     return ((n+1)//2, n//2)
 
 def bathroom_stalls():
     N, K = map(int, raw_input().strip().split())
-    #  we can reduce the problem by the following rules:
-    #  - bathroom_stalls(2N+2, 2K+2) = bathroom_stalls(N+1, K+1)
-    #  - bathroom_stalls(2N+3, 2K+2) = bathroom_stalls(N+1, K+1)
-    #  - bathroom_stalls(2N+2, 2K+3) = bathroom_stalls(N,   K+1)
-    #  - bathroom_stalls(2N+3, 2K+3) = bathroom_stalls(N+1, K+1)
-    #  - bathroom_stalls(2N+2,    1) = (N+1, N)
-    #  - bathroom_stalls(2N+1,    1) = (N,   N)
-
     while K > 1:
         M, m = max_min(N-1)
         K -= 1
-        N = M if K % 2 else m
+        N = M if K % 2 == 1 else m
         K = (K+1)//2
-            
+
     return max_min(N-1)
 
 for case in xrange(input()):
