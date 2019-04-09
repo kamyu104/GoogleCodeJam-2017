b984afa716fce324ee689c57ec0ca74ed1ddb92e
kamyu <kamyu104@users.noreply.github.com>
2019-04-09 22:24:47 +0800

    Update bathroom-stalls.py

--- Qualification Round/bathroom-stalls.py
+++ Qualification Round/bathroom-stalls.py
@@ -36,7 +36,8 @@ def bigger_smaller(n):
 
 def bathroom_stalls3():  # simplified by bit operation of bathroom_stalls2
     N, K = map(int, raw_input().strip().split())
-    return bigger_smaller((N-K) >> (K.bit_length()-1))  # bigger_smaller((N-K) // 2**(K.bit_length()-1))
+    return bigger_smaller((N-K) >> (K.bit_length()-1))  # this implies that 
+                                                        # (N-K) // 2**(K.bit_length()-1) = max(LS, RS) + min(LS, RS)
 
 def bathroom_stalls2():  # implemented by conclusion
     N, K = map(int, raw_input().strip().split())
