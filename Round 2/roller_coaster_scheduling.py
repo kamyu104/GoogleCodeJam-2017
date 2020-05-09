d25d8a6059a92ddad0e1235db6ad609b6327bd05
Allen Liu <kamyu104@gmail.com>
2020-05-09 17:24:43 +0800

    update

--- Round 2/roller_coaster_scheduling.py
+++ Round 2/roller_coaster_scheduling.py
@@ -3,8 +3,8 @@
 # Google Code Jam 2017 Round 2 - Problem B. Roller Coaster Scheduling
 # https://codingcompetitions.withgoogle.com/codejam/round/0000000000201900/0000000000201845
 #
-# Time:  O(N)
-# Space: O(N)
+# Time:  O(N + M)
+# Space: O(N + M)
 #
 
 from collections import defaultdict
@@ -19,18 +19,18 @@ def roller_coaster_scheduling():
         P, B = map(int, raw_input().strip().split())
         seat_count[P] += 1
         customer_count[B] += 1
-    y, seat_count_not_after_k = max(customer_count.itervalues()), 0
+    min_ride, accu = max(customer_count.itervalues()), 0
     for k in xrange(1, N+1):
         if k not in seat_count:
             continue
-        seat_count_not_after_k += seat_count[k]
-        y = max(y, ceil(seat_count_not_after_k, k))
-    z = 0
+        accu += seat_count[k]
+        min_ride = max(min_ride, ceil(accu, k))
+    min_promote = 0
     for k in xrange(1, N+1):
         if k not in seat_count:
             continue
-        z += max(seat_count[k]-y, 0)
-    return "{} {}".format(y, z)
+        min_promote += max(seat_count[k]-min_ride, 0)
+    return "{} {}".format(min_ride, min_promote)
 
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, roller_coaster_scheduling())
