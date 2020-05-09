5e451868b0ab652d3862a39b3695d1b686784b10
Allen Liu <kamyu104@gmail.com>
2020-05-09 18:35:12 +0800

    update

--- Round 2/roller_coaster_scheduling.py
+++ Round 2/roller_coaster_scheduling.py
@@ -12,6 +12,14 @@ from collections import defaultdict
 def ceil(a, b):
     return (a-1)//b+1
 
+def prefix_sum_gen(N, seat_count):
+    accu = 0
+    for k in xrange(1, N+1):
+        if k not in seat_count:
+            continue
+        accu += seat_count[k]
+        yield k, accu
+
 def roller_coaster_scheduling():
     N, C, M = map(int, raw_input().strip().split())
     seat_count, customer_count = defaultdict(int), defaultdict(int)
@@ -19,18 +27,8 @@ def roller_coaster_scheduling():
         P, B = map(int, raw_input().strip().split())
         seat_count[P] += 1
         customer_count[B] += 1
-    min_ride, accu = max(customer_count.itervalues()), 0
-    for k in xrange(1, N+1):
-        if k not in seat_count:
-            continue
-        accu += seat_count[k]
-        min_ride = max(min_ride, ceil(accu, k))
-    min_promote = 0
-    for k in xrange(1, N+1):
-        if k not in seat_count:
-            continue
-        min_promote += max(seat_count[k]-min_ride, 0)
-    return "{} {}".format(min_ride, min_promote)
+    min_ride = max(max(ceil(accu, k) for k, accu in prefix_sum_gen(N, seat_count)), max(customer_count.itervalues()))
+    return "{} {}".format(min_ride, sum(max(seat_count[k]-min_ride, 0) for k in xrange(1, N+1) if k in seat_count))
 
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, roller_coaster_scheduling())
